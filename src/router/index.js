5ac35da7e98f25167a1c6d70c46e537a5d3402e5
mangoSu <acid7820@gmail.com>
2022-04-14 19:19:56 +0800

    feat: fetch csv before enter PoliticalContribution at router/index.js
    1. route /home redirect to route /political-contribution
    2. import component PoliticalContribution.vue
    3. fetch csv before enter PoliticalContribution

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
+import pinia from "@/stores/pinia";
+import { useCandidateStore } from "@/stores/candidate";
 
+const candidateStore = useCandidateStore(pinia);
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
       name: "home",
+      redirect: () => {
+        return { name: "PoliticalContribution" };
+      },
       component: () => import("../views/Home.vue"),
+    },
+    {
+      path: "/political-contribution",
+      name: "PoliticalContribution",
+      component: () => import("../views/PoliticalContribution.vue"),
+      beforeEnter: async () => {
+        await candidateStore.fetchAllPoliticalContributionsIncome();
+      },
     },
   ],
 });
