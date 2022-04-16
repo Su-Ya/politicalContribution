d4921e39b7a19317e87123601d248d54cd792fc4
mangoSu <acid7820@gmail.com>
2022-04-17 05:01:04 +0800

    feat: add button、line、active scss at _component.scss、index.scss、PoliticalContribution.vue

--- src/views/PoliticalContribution.vue
+++ src/views/PoliticalContribution.vue
@@ -20,8 +20,20 @@ let modal = ref();
     <div>
       <h2 class="modals-title">請選擇分析模式</h2>
       <div class="modals">
-        <button @click="modal = 'pk'">PK</button>
-        <button @click="modal = 'single'">個人資料</button>
+        <button
+          class="button outline-default"
+          :class="{ active: modal === 'pk' }"
+          @click="modal = 'pk'"
+        >
+          PK
+        </button>
+        <button
+          class="button outline-default"
+          :class="{ active: modal === 'single' }"
+          @click="modal = 'single'"
+        >
+          個人資料
+        </button>
       </div>
     </div>
     <PKCandidates
@@ -41,17 +53,6 @@ let modal = ref();
   margin: auto;
   .modals-title {
     margin-bottom: 0;
-  }
-  .modals {
-    button {
-      font-size: 1rem;
-      line-height: 1;
-      padding: 0.4em 0.6em;
-      margin: 0.4em;
-      background-color: #fff;
-      border: 1px solid red;
-      border-radius: 8px;
-    }
   }
 }
 </style>
